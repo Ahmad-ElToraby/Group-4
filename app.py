b52b9d1336c8f7626303c4ce5bdb32adb490aa1e
alaa <alaaazazi21@gmail.com>
2025-11-16 09:36:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
 import streamlit as st
 import requests
 from PIL import Image
+
+st.title("Emotion Detection App ")
+
+uploaded = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+if uploaded:
+    # Show image
+    img = Image.open(uploaded)
+    st.image(img, caption="Uploaded Image", width=250)
+
+    if st.button("Predict Emotion"):
+        files = {"file": uploaded.getvalue()}
+
+        # Call FastAPI endpoint
+        res = requests.post("http://127.0.0.1:8000/predict", files=files)
+        # json file for emotion
+
+        emotion = res.json()["emotion"]
+        st.success(f"Detected Emotion: {emotion}")
+        #
